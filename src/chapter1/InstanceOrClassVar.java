5f60c380234997ad7b59f763bc0ed833cf8a8e6d
myimyaem <137817981+myimyaem@users.noreply.github.com>
2023-10-25 09:15:29 +0000

    day 9 source codes

---
+++ src/chapter1/InstanceOrClassVar.java
@@ -0,0 +1,26 @@
+// end of chapter1
+package chapter1;
+import java.util.Random;
+public class InstanceOrClassVar {
+    int y;
+    String name;
+    boolean isRunning;
+    public static void main(String[] args) {
+        InstanceOrClassVar programm1 = new InstanceOrClassVar();
+        System.out.println(programm1.y);
+        System.out.println(programm1.name);
+        System.out.println(programm1.isRunning);
+        InstanceOrClassVar programm2 = new InstanceOrClassVar();
+        InstanceOrClassVar programm3 = programm1;
+        programm3.y=900;
+        programm1.y=800;
+        System.out.println(programm1.y+" *** "+programm3.y);
+        InstanceOrClassVar programm4;
+        programm1 = null;
+        programm3=null;
+        System.gc();
+        System.out.println("after gc");
+    }
+   
+    
+}
---
+++ src/chapter1/LocalVar.java
@@ -0,0 +1,10 @@
+package chapter1;
+
+public class LocalVar {
+    public static void main(String[] args) {
+        int y = 10;
+        int x; 
+        x = 5;
+        System.out.println(x+y);
+    }
+}
