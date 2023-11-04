a48595d2ce07ce218f377a38ab3e80a3b0fc7bbc
myimyaem <137817981+myimyaem@users.noreply.github.com>
2023-11-04 08:31:13 +0000

    task113

---
+++ src/taskguide/Task113.java
@@ -0,0 +1,21 @@
+package taskguide;
+
+class Task113 {
+    public static void main(String[] args) {
+        int check_number = 10;
+        String message;
+        for(int i=0; i<check_number;i++){
+            message = (i%2==0) ? i+" is even number" : i+" is odd number";
+            System.out.println(message);
+        }
+    }
+
+    
+}
+
+// Create an int variable and name it check_number with an initial value of 10.
+// Create a String variable and name it message.
+// Use check_number as a condition on how many loops the program can print.
+// Create a loop that will check and print each number if odd or even numbers using this syntax:  message = (i%2==0) ? i+" is even number" : i+" is odd number";
+// The next line should print message value.
+// Submit the link of your source code.
