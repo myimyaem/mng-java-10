1eb8bb0f8b10aa01767c5eaf2f0b5d1cde9e20e3
myimyaem <137817981+myimyaem@users.noreply.github.com>
2023-11-08 08:22:34 +0000

    day 18 part 1

---
+++ src/chapter3/DateTimeSample.java
@@ -0,0 +1,10 @@
+package chapter3;
+import java.time.*;
+
+public class DateTimeSample {
+    public static void main(String[] args) {
+        System.out.println(LocalDate.now());
+        System.out.println(LocalTime.now());
+        System.out.println(LocalDateTime.now());
+    }    
+}
